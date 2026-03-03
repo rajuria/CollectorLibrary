568695705fa4ada59c22d8460d6ff057453351b2
rajuria <rajuria@unitec.edu>
2026-03-02 18:20:41 -0600

    Agregadas correctamente las referencias para las relaciones

--- migrations/20260225235902-create-books.js
+++ migrations/20260225235902-create-books.js
@@ -10,21 +10,26 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       Title: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       AuthorID: {
+        references: {model: 'Autores',key: 'id'},
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       Genre: {
         type: Sequelize.STRING
       },
       PublicationYear: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       isbn: {
         type: Sequelize.STRING
       },
       PageCount: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       createdAt: {
--- models/books.js
+++ models/books.js
@@ -4,14 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Books extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-      Books.belongsTo(models.Autores, { foreignKey: 'AuthorID' });
+      Books.belongsTo(models.Autores, { foreignKey: 'AuthorID', as: 'Author' });
+      Books.hasOne(models.ReadingStatuses, {foreignKey: 'BookID',as: 'ReadingStatuses'});
     }
   }
   Books.init({
