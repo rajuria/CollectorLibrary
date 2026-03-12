dac48d9e26858e6fa6908a37258e004bf5854c29
rajuria <rajuria@unitec.edu>
2026-03-11 18:43:33 -0600

    Agregados endpoints de Books
    Agregado endpoint getByISBN & createBook

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var authorsRouter = require('./routes/authors');
 var booksRouter = require('./routes/books');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
